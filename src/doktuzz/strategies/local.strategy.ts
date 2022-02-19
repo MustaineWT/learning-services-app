fa221ceba9a4a9206661ad721009999e4cecfe33
MustaineWT <wmtorresb@gmail.com>
2022-02-18 22:07:35 -0500

    fix route data

--- src/doktuzz/strategies/local.strategy.ts
+++ src/doktuzz/strategies/local.strategy.ts
@@ -1,22 +1,25 @@
+import { Strategy } from 'passport-local';
 import { Injectable,UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { DoktuzService } from '../doktuz.service';
 
 @Injectable()
-export class LocalStrategy {
-  constructor() {
-    ({
+export class LocalStrategy extends PassportStrategy(Strategy){
+  constructor(private readonly doktuzService: DoktuzService) {
+    super({
       startDateField: 'startDate', 
       endDateField: 'endDate',
     });
   }
 
   async getAllWithRangeDate(startDate: Date, endDate: Date) {
-    const data = await this.getAllWithRangeDate(startDate, endDate);
+    const data = await this.doktuzService.getAllWithRangeDate(startDate, endDate);
     if (!data)
       throw new UnauthorizedException('Data not range information.');
     return data;
   }
   async getAllWithNameOrCompany(name: string, company: string) {
-    const data = await this.getAllWithNameOrCompany(name, company);
+    const data = await this.doktuzService.getAllWithNameOrCompany(name, company);
     if (!data)
       throw new UnauthorizedException('Data not information because error name or company found.');
     return data;
