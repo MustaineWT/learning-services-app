0182ee13738f4476581e07f2af816020cb484b5c
MustaineWT <wmtorresb@gmail.com>
2022-02-18 20:08:59 -0500

    fix learningApp

--- src/doktuzz/doktuz.controller.ts
+++ src/doktuzz/doktuz.controller.ts
@@ -6,7 +6,9 @@ import {
 } from '@nestjs/common';
 //import { GetDoktuzWithRangeDateTimeDto } from './dtos';
 import { DoktuzService } from './doktuz.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Learning Routes')
 @Controller('doktuzz')
 export class DoktuzController {
   constructor(private readonly doktuzService: DoktuzService) {}
